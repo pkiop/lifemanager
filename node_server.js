a9ae9b34edc700c355842a43807e2f9f62d8fea5
Park Sang Shin <34783156+pkiop@users.noreply.github.com>
2020-04-22 22:45:13 +0900

    Update node_server.js

--- node_server.js
+++ node_server.js
@@ -6,7 +6,6 @@ var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    console.log("queryData : ", queryData);
     if(pathname === '/') {
         if(queryData.id === undefined) {
             topic.home(request, response);
