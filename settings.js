d3fed2ec2bff2f7028c77bac8f97d204cd842aed
Park Sang Shin <34783156+pkiop@users.noreply.github.com>
2020-05-05 20:22:19 +0900

    money_study category updated and auto option add

--- settings.js
+++ settings.js
@@ -1,4 +1,6 @@
 module.exports = {
     port_num: 3000,
-    categorys: ['develop', 'study', 'book_reading', 'exercise', 'work', 'english', 'chinese', 'etc']
+    categorys: ['develop', 'study', 'book_reading', 'exercise', 'work', 'english', 'chinese', 'etc', 'money_study'],
+    categorys_korean: ['개발', '공부', '책', '운동', '회사', '영어', '중국어', '기타', '돈공부']
+
 }
