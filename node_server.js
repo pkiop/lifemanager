dc40edeea671ac3c518bae44de7fabba768f3cc4
pkiop <vivezdv@gmail.com>
2020-05-17 10:17:38 +0900

    temp_express_versionup

--- node_server.js
+++ node_server.js
@@ -4,8 +4,54 @@ var topic = require('./lib/topic');
 var settings = require('./settings');
 var portnum = settings.port_num;
 var fs = require('fs');
+var passport = require('passport');
+var express = require('express');
+var helmet = require('helmet')
 
+var app = express();
+app.use(helmet());
 
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var helmet = require('helmet');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(compression());
+
+/*
+
+app.use(express.static('public'));
+
+이미지, CSS 파일 및 JavaScript 파일과 같은 정적 파일을 제공하려면 Express의 기본 제공 미들웨어 함수인 express.static을 사용하십시오.
+정적 자산이 포함된 디렉토리의 이름을 express.static 미들웨어 함수에 전달하면 파일의 직접적인 제공을 시작할 수 있습니다. 예를 들면, 다음과 같은 코드를 이용하여 public이라는 이름의 디렉토리에 포함된 이미지, CSS 파일 및 JavaScript 파일을 제공하십시오.
+
+http://localhost:3000/images/kitten.jpg
+http://localhost:3000/css/style.css
+http://localhost:3000/js/app.js
+http://localhost:3000/images/bg.png
+http://localhost:3000/hello.html
+
+Express는 정적 디렉토리에 대해 상대적으로 파일을 검색하며, 따라서 정적 디렉토리의 이름은 URL의 일부가 아닙니다.
+
+*/
+var indexRouter = require('./routes/index');
+
+app.use('/', indexRouter);
+
+app.use(function(req, res, next) {
+    res.status(404).send("Sorry can't find that!");
+});
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Somthing broke!');
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!')
+});
+/*
 var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -72,6 +118,7 @@ var app = http.createServer(function(request, response) {
 });
 app.listen(portnum);
 
+*/
 // app2.get('/logo', function(request, response) {
 //     fs.readFile('/pic/logo.png', function(error, data) {
 //         response.writeHead(200, {'Content-Type': 'text/html'});
---
+++ routes/index.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var router = express.Router();
+var topic = require('../lib/topic');
+var url = require('url')
+
+router.get('/', function (req, res) {
+    topic.home(req, res);
+});
+
+router.get('/puttime_process', function (req, res) {
+    topic.puttime_process(req, res);
+});
+router.get('/selecttable', function (req, res) {
+    var _url = req.url;
+    var queryData = url.parse(_url, true).query;
+    topic.selecttable(req, res, queryData);
+});
+router.get('/showNupdatetable', function (req, res) {
+    var _url = req.url;
+    var queryData = url.parse(_url, true).query;
+
+    topic.showNupdatetable(req, res, queryData);
+});
+router.post('/showtable_process', function (req, res) {
+    console.log('show_table_process');
+    topic.showtable_process(req, res);
+});
+router.get('/intervalrecodes', function (req, res) {
+    topic.select_interval(req, res);
+});
+router.get('/showtotalrecodes', function (req, res) {
+    topic.showtotalrecodes(req, res);
+});
+router.get('/todaytable_process', function (req, res) {
+    topic.todaytable_process(req, res);
+}); 
+router.get('/update_process', function(req, res){
+    topic.update_process(req, res);
+}); 
+router.get('/delete_process', function(req, res){
+    topic.delete_process(req, res);
+});
+router.get('/confirm_process', function(req, res){
+    topic.confirm_process(req, res);
+}) 
+router.get('/selectSleepTime',function(req,res){
+    var _url = req.url;
+    var queryData = url.parse(_url, true).query;
+
+    topic.selectSleepTime(req, res, queryData);
+});
+router.get('/maketable_process', function(req, res) {
+    topic.maketable_process(req, res);
+}); 
+router.get('/interval_process', function(req, res){
+    topic.interval_process(req, res);
+}) 
+router.get('/showcategory', function(req, res) {
+    topic.showcategory(req, res);
+});
+router.get('/category_process', function(req, res){
+    var _url = req.url;
+    var queryData = url.parse(_url, true).query;
+
+    if (queryData.categoryValue === undefined) {
+        res.writeHead(404);
+        res.end('Not found');
+    }
+    else {
+        topic.category_process(req, res, queryData.categoryValue);
+    }
+}); 
+  
+module.exports = router;
