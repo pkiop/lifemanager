91144978db4444fcec2e94616586d50f797d7dc3
pkiop <vivezdv@gmail.com>
2020-05-20 23:34:02 +0900

    goot

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,8 @@
 module.exports = {
   apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
+    script: 'node_server.js',
+    watch: '.',
+    ignore_watch: ['./lowdb.json']
+  }, ],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
 };
