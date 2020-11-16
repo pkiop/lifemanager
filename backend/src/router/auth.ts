116f9e4d8111710123ad08274f1a5c8747279c0c
pkiop <pkiop@naver.com>
2020-11-17 07:41:52 +0900

    chore: api 폴더구조 개선

--- backend/src/router/auth.ts
+++
@@ -1,9 +0,0 @@
-import Koa from 'koa';
-import Router from 'koa-router';
-import githubController from '../controller/github';
-
-const router = new Router();
-
-router.get('/github/callback', githubController.OAuth);
-
-export default router;
