33b301854a3a162f3a9074cbcefe8ce55c894f5a
pkiop <pkiop@naver.com>
2020-11-18 13:16:25 +0900

    Feat: Write test API

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,19 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 import allRouter from '@Router/index';
+import cors from '@koa/cors';
+
+const agb = '???';
 
 const app = new Koa();
+app.use(cors());
 const router = new Router();
 
 router.use(allRouter.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
-app.use(async (ctx:Koa.Context)=> {
-  ctx.body = `<Button onclick=(function() {alert('하이')})>하이</Button>`;
+app.use(async (ctx: Koa.Context) => {
+  ctx.body = "<Button onclick=(function() {alert('하이')})>하이</Button>";
 });
 
 app.listen(3000);
