f76de0df8085ba605364ada8f44bf5688a47f95a
pkiop <pkiop@naver.com>
2020-11-18 09:10:52 +0900

    Feat: Add sample GitHubLogin Button story

---
+++ frontend/src/component/UI/atoms/GitHubLogin/GitHubLogin.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import GitHubButton, { Props } from './index';
+
+export default {
+  title: 'GitHubButton',
+  component: GitHubButton,
+  parameters: { actions: { argTypesRegex: '^on.*' } },
+  argTypes: { onClick: { action: 'clicked' } },
+};
+
+const Template: Story<Props> = (args) => <GitHubButton {...args} />;
+
+export const Pair = Template.bind({});
+Pair.args = {
+  title: 'hello',
+};
+
+export const HELLO = () => {
+  const subTitle = 'HELLO';
+  return <GitHubButton title={subTitle} />;
+};
+
+export const BYE = () => {
+  const subTitle = 'BYE';
+  return <GitHubButton title={subTitle} />;
+};
