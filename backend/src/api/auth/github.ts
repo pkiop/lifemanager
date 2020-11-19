991222f33555eaf0d2b422d7b28b57152db0e7ef
pkiop <pkiop@naver.com>
2020-11-19 23:14:35 +0900

    Feat: Add GitHub OAuth callback, login controller

--- backend/src/api/auth/github.ts
+++ backend/src/api/auth/github.ts
@@ -1,21 +1,31 @@
 import Koa from 'koa';
-import fetch from 'node-fetch';
 import env from 'dotenv';
+import qs from 'qs';
+import axios from 'axios';
+
 env.config();
 
-const OAuth = async (ctx: Koa.Context, next:Function) => {
-  console.log("redirect!!!!/n\n\n\n\n\n\n\n");
-  ctx.body = "안뇽하세요";
-}
+const OAuth = (ctx: Koa.Context, next: Function) => {
+  ctx.body = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${process.env.GITHUB_REDIRECT_URL}&scope=user:email`;
+};
 
-const authorize = async (ctx: Koa.Context, next:Function) => {
-  const GitHubAuthorizeUrl = 'https://github.com/login/oauth/authorize';
-  const url = `${GitHubAuthorizeUrl}?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${process.env.GITHUB_REDIRECT_URL}&scope=user:email`;
-  console.log(url);
-  const res = await fetch(url);
-  console.log(res);
-  console.log('body : ', await res.body);
-  ctx.body = res;
-}
+const OAuthCallback = async (ctx: Koa.Context) => {
+  const host = 'https://github.com/login/oauth/access_token?';
+  const queryString = qs.stringify({
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_CLIENT_SECRET,
+    code: ctx.request.query.code,
+    redirect_uri: process.env.GITHUB_REDIRECT_URL,
+    state: process.env.OAUTH_STATE,
+  });
+  try {
+    const res = await axios.get(`${host}${queryString}`);
+    const token = qs.parse(res.data).access_token;
+    ctx.cookies.set('accessToken', token as string);
+    ctx.redirect(`${process.env.FRONT_SERVER_INDEXPAGE}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-export default { OAuth, authorize }
+export default { OAuth, OAuthCallback };
