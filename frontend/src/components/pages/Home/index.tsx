80d0098152acd31186ca32d31a078c550f560c49
pkiop <pkiop@naver.com>
2020-11-20 23:08:47 +0900

    Feat: Add Awaypage routing button

--- frontend/src/components/pages/Home/index.tsx
+++ frontend/src/components/pages/Home/index.tsx
@@ -1,9 +1,29 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useReducer } from 'react';
+import { userStore } from '@Stores/user';
+import { observer } from 'mobx-react';
+import {
+  Redirect,
+} from 'react-router-dom';
 
-const App: FC = () => (
-  <>
-    <h1>여기는 Home</h1>
-  </>
-);
+const App: FC = () => {
+  const [isClicked, setIsClicked] = useReducer((state) => !state, false);
+  useEffect(() => {
 
-export default App;
+  }, [isClicked]);
+  if (isClicked === true) {
+    return <Redirect
+      to={{
+        pathname: '/away',
+      }}
+    />;
+  }
+  return (
+    <>
+      <h1>여기는 Home</h1>
+      <h2>{userStore.data.username}</h2>
+      <button onClick={() => { console.log('onClick'); userStore.logIn({ username: `${userStore.data.username}1` }); }} >클릭</button>
+      <button onClick={() => setIsClicked()} >Away으로</button>
+    </>
+  );
+};
+export default observer(App);
