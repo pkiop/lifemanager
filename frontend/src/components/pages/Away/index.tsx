18ac788e68534b8fa165155a2372b443a568a280
pkiop <pkiop@naver.com>
2020-11-21 18:37:06 +0900

    Feat: Add test env for TR API

--- frontend/src/components/pages/Away/index.tsx
+++ frontend/src/components/pages/Away/index.tsx
@@ -1,16 +1,38 @@
-import React, { FC, useEffect, useReducer } from 'react';
+import React, {
+  FC, useEffect, useState, useReducer,
+} from 'react';
 import { userStore } from '@Stores/user';
 import { observer } from 'mobx-react';
 import {
   Redirect,
 } from 'react-router-dom';
+import Axios from 'axios';
+
+const getHandler = async (setReceiveData: React.Dispatch<React.SetStateAction<string>>) => {
+  const res = await Axios(`${process.env.APISERVER_HOST}/api/timeRecode`);
+  console.log('res : ', res.data[0].test);
+  setReceiveData(res.data[0].test);
+};
+
+const postHandler = async (setReceiveData: React.Dispatch<React.SetStateAction<string>>) => {
+  const res = await Axios(`${process.env.APISERVER_HOST}/api/timeRecode`);
+  console.log('res : ', res.data);
+  setReceiveData(res.data as string);
+};
+
+const putHandler = () => {
+
+};
+const deleteHandler = () => {
+
+};
 
 const App: FC = () => {
   const [isClicked, setIsClicked] = useReducer((state) => !state, false);
-  console.log('dcdc ', document.cookie);
+  const [receiveData, setReceiveData] = useState('');
   useEffect(() => {
 
-  }, [isClicked]);
+  }, [isClicked, receiveData]);
   if (isClicked === true) {
     return <Redirect
       to={{
@@ -23,6 +45,11 @@ const App: FC = () => {
       <h1>여기는 away</h1>
       <h2>{userStore.data.username}</h2>
       <button onClick={() => setIsClicked()} >Home으로</button>
+      <button onClick={() => getHandler(setReceiveData)}>GET TR</button>
+      <button onClick={() => {}}>POST TR</button>
+      <button onClick={() => {}}>PUT TR</button>
+      <button onClick={() => {}}>DELETE TR</button>
+      <h2>{receiveData}</h2>
     </>
   );
 };
