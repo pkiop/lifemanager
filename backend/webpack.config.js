b40065e9cc40b2d56cbc9c8a907c568f0e344671
pkiop <pkiop@naver.com>
2020-11-21 18:35:43 +0900

    Fix: Fix webpack config auth lint

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,34 +1,35 @@
 const path = require('path');
+
 module.exports = {
-  name: 'ts-back-setting', mode: 'development', // "production" | "development" | "none" 
-  devtool:'eval', // source-map hidden-source-map 
-  resolve: { 
-    modules: ['node_modules'], 
-    extensions: ['.ts', 'json', '.jsx', '.js'], 
-    alias: { 
-      "@Controller": path.resolve(__dirname, 'src/controller'), 
-      "@Router": path.resolve(__dirname, 'src/router'), 
-      "@Api": path.resolve(__dirname, 'src/api'), 
-    } ,
-  }, 
-  entry: { 
-    index: ['./src/app.ts'] 
-  }, 
-  module: { 
+  name: 'ts-back-setting',
+  mode: 'development', // "production" | "development" | "none"
+  devtool: 'eval', // source-map hidden-source-map
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.ts', 'json', '.jsx', '.js'],
+    alias: {
+      '@Controller': path.resolve(__dirname, 'src/controller'),
+      '@Router': path.resolve(__dirname, 'src/router'),
+      '@Api': path.resolve(__dirname, 'src/api'),
+    },
+  },
+  entry: {
+    index: ['./src/app.ts'],
+  },
+  module: {
     rules: [
-      { 
-        test: /\.ts$/, 
-        use: ['ts-loader'], 
-        exclude: ["/node_modules"] 
-      }, 
-      ] 
-    }, 
-  optimization:{}, 
-  output:{ 
-    path:path.join(__dirname,'./dist'), 
-    filename:'[name].js' 
-  }, 
+      {
+        test: /\.ts$/,
+        use: ['ts-loader'],
+        exclude: ['/node_modules'],
+      },
+    ],
+  },
+  optimization: {},
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: '[name].js',
+  },
   target: 'node',
 
-}
-
+};
