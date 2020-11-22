ccb42726203730d27c70e54e978d031f14b95d3d
pkiop <pkiop@naver.com>
2020-11-22 23:31:58 +0900

    Feat: Parsing post request body data

--- backend/src/controller/api/timeRecode/index.ts
+++ backend/src/controller/api/timeRecode/index.ts
@@ -1,20 +1,19 @@
 import Koa from 'koa';
 
-export const get = (ctx: Koa.Context, next: Function) => {
+export const get = (ctx: Koa.Context) => {
   ctx.body = [{ test: 'get' }];
 };
 
-export const post = (ctx: Koa.Context, next: Function) => {
-  console.log('what is ctx.request in post : ', ctx.request);
-  ctx.body = [{ test: ctx.request }];
+export const post = (ctx: Koa.Context) => {
+  ctx.body = [{ test: ctx.request.body }];
 };
 
-export const put = (ctx: Koa.Context, next: Function) => {
+export const put = (ctx: Koa.Context) => {
   console.log('what is ctx.request in put : ', ctx.request);
   ctx.body = [{ test: ctx.request }];
 };
 
-export const del = (ctx: Koa.Context, next: Function) => {
+export const del = (ctx: Koa.Context) => {
   console.log('what is ctx.request in delete: ', ctx.request);
   ctx.body = [{ test: ctx.request }];
 };
