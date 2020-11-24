30b4622a20b111685c0bffc37a22a41fb061b10e
pkiop <pkiop@naver.com>
2020-11-24 09:23:16 +0900

    Feat: Add input story

---
+++ frontend/src/components/UI/atoms/Input/index.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Input from '.';
+
+export default {
+  title: 'Atoms/Input',
+  component: Input,
+};
+
+export const HELLO = () => <Input value={'HELLO'} placeholder={'하이'} />;
