5bfe9fa1a1875a010ebeeeb258db5e33d9f66b78
pkiop <pkiop@naver.com>
2020-11-27 23:44:40 +0900

    Feat: Change to import svg, png file

--- frontend/.storybook/webpack.config.js
+++ frontend/.storybook/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = ({ config, mode }) => {
     '@Organisms': path.resolve(__dirname, '../','src/components/organisms'),
     '@Templates': path.resolve(__dirname, '../','src/components/templates'),
     '@Pages': path.resolve(__dirname, '../','src/components/pages'),
-    '@Images': path.resolve(__dirname, '../','images'),
+    '@Images': path.resolve(__dirname, '../','src/images'),
     '@Styles': path.resolve(__dirname, '../','src/styles'),
     '@Utils': path.resolve(__dirname, '../','src/utils'),
     '@Hooks': path.resolve(__dirname, '../','Hooks'),
