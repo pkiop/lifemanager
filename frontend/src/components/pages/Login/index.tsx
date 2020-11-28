8fefadbf868f737e2f1ccfede1e76d4088fcf310
pkiop <pkiop@naver.com>
2020-11-28 14:19:50 +0900

    Fix: Add Headerbar, GithubLogin button

--- frontend/src/components/pages/Login/index.tsx
+++ frontend/src/components/pages/Login/index.tsx
@@ -1,32 +1,30 @@
 import React, { FC } from 'react';
 import axios from 'axios';
-import Button from '@Atoms/Button';
 import { observer } from 'mobx-react';
 import { userStore } from '@Stores/user';
-import {
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import GitHubLogo from '@Images/GitHub.png';
+import { Redirect } from 'react-router-dom';
+import * as S from './style';
 
 const githubLogin = async () => {
   const res = await axios.get(`${process.env.APISERVER_HOST}/api/auth/github`);
   window.location.href = res.data;
 };
 
-const sampleLogin = () => {
-  userStore.logIn({
-    username: 'newUser',
-  });
-};
-
-const App: FC<any> = ({ from = { pathname: '/', search: '', hash: '' } }) => {
+const App = ({
+  from = { pathname: '/', search: '', hash: '' },
+  className,
+}: any) => {
   if (userStore.data.username !== 'noUser') return <Redirect to={from} />;
   return (
-    <>
-      <h1>여기는 로그인 페이지</h1>
-      <Button onClick={githubLogin}> {'GitHubLogin'}</Button>
-      <Button onClick={sampleLogin}> {'Sample Login'}</Button>
-    </>
+    <S.LoginPage>
+      <S.HeaderBar className={className} />
+      <S.GitHubLogin
+        icon={GitHubLogo}
+        ButtonTitle={'Login With GitHub'}
+        onClick={githubLogin}
+      />
+    </S.LoginPage>
   );
 };
 
