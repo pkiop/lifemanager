9306e76c2ca0c847a1e5e240829b0d5214de3bb2
pkiop <pkiop@naver.com>
2020-11-28 17:34:41 +0900

    Feat: Write User model scheme

---
+++ backend/src/models/user.ts
@@ -0,0 +1,39 @@
+import {
+  Schema, model, Document,
+} from 'mongoose';
+
+const UserSchema = new Schema({
+  userId: {
+    type: String,
+  },
+  GitHubProfileUrl: {
+    type: String,
+  },
+  goalActiveTime: {
+    type: Array,
+    default: [13, 0],
+  },
+  sleepTime: {
+    type: Array,
+    default: [0, 30],
+  },
+  activeCategoryList: {
+    type: Array,
+    default: ['develop', 'reading'],
+  },
+  excludeCategoryList: {
+    type: Array,
+    default: ['nap', 'meal'],
+  },
+});
+
+export interface User extends Document {
+  userId: string;
+  GitHubProfileUrl: string;
+  goalActiveTime: Array<number>;
+  sleepTime: Array<number>;
+  activeCategoryList: Array<string>;
+  excludeCategoryList: Array<string>;
+}
+
+export const UserModel = model<User>('users', UserSchema);
