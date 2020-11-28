f3c53e691bee64fb9a8da45fc48597818a7bbfe7
pkiop <pkiop@naver.com>
2020-11-28 14:16:50 +0900

    Fix: Change Organisms wrong alias path

--- frontend/.storybook/webpack.config.js
+++ frontend/.storybook/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = ({ config, mode }) => {
     '@Components': path.resolve(__dirname, '../' , 'src/components'),
     '@Atoms': path.resolve(__dirname, '../','src/components/UI/atoms'),
     '@Molecules': path.resolve(__dirname, '../','src/components/UI/molecules'),
-    '@Organisms': path.resolve(__dirname, '../','src/components/organisms'),
+    '@Organisms': path.resolve(__dirname, '../','src/components/UI/organisms'),
     '@Templates': path.resolve(__dirname, '../','src/components/templates'),
     '@Pages': path.resolve(__dirname, '../','src/components/pages'),
     '@Images': path.resolve(__dirname, '../','src/images'),
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
       '@Components': path.resolve(__dirname, 'src/components'),
       '@Atoms': path.resolve(__dirname, 'src/components/UI/atoms'),
       '@Molecules': path.resolve(__dirname, 'src/components/UI/molecules'),
-      '@Organisms': path.resolve(__dirname, 'src/components/organisms'),
+      '@Organisms': path.resolve(__dirname, 'src/components/UI/organisms'),
       '@Templates': path.resolve(__dirname, 'src/components/templates'),
       '@Pages': path.resolve(__dirname, 'src/components/pages'),
       '@Images': path.resolve(__dirname, 'src/images'),
