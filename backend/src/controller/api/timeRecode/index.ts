45b3d4b91caca3536c0a79c6432d3b65c6f9af7c
pkiop <pkiop@naver.com>
2020-11-28 18:27:44 +0900

    Feat: Add TimeRecode get, post controller

--- backend/src/controller/api/timeRecode/index.ts
+++ backend/src/controller/api/timeRecode/index.ts
@@ -1,11 +1,16 @@
 import Koa from 'koa';
+import { addOneTimeRecode, getAllTimeRecode } from 'service/timeRecode';
 
-export const get = (ctx: Koa.Context) => {
-  ctx.body = [{ test: 'get' }];
+export const get = async (ctx: Koa.Context) => {
+  const res = await getAllTimeRecode();
+  console.log('res : ', res);
+  ctx.body = [{ data: res }];
 };
 
-export const post = (ctx: Koa.Context) => {
-  ctx.body = [{ test: ctx.request.body }];
+export const post = async (ctx: Koa.Context) => {
+  await addOneTimeRecode(ctx.request.body);
+  ctx.status = 201;
+  ctx.body = { message: 'success' };
 };
 
 export const put = (ctx: Koa.Context) => {
