470775a44275cce7c16d5e5084aff8e40d7bfd71
pkiop <pkiop@naver.com>
2020-11-29 22:21:27 +0900

    Refector: Change locate time function to util/time.ts

--- frontend/src/components/UI/molecules/Recode/index.tsx
+++ frontend/src/components/UI/molecules/Recode/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeTwoNumber } from '@Utils/time';
+import { makeTwoNumber, nowHourMin, calNowTime } from '@Utils/time';
 import * as S from './style';
 
 export interface Props {
@@ -10,30 +10,6 @@ export interface Props {
   isActive: boolean;
   className?: string;
 }
-
-const nowHourMin = () => {
-  const nowTime = new Date();
-  return [nowTime.getHours(), nowTime.getMinutes()];
-};
-
-const calNowTime = (startTime: number[], endTime: number[]) => {
-  const [startHour, startMin] = startTime;
-  const [endHour, endMin] = endTime;
-
-  if (endHour < startHour) {
-    if (startMin > endMin) {
-      return [endHour + 23 - startHour, startMin - endMin];
-    }
-    return [endHour + 24 - startHour, endMin - startMin];
-  }
-  if (startMin > endMin) {
-    if (endHour === startHour) {
-      return [endHour + 23 - startHour, startMin - endMin];
-    }
-    return [endHour - 1 - startHour, startMin - endMin];
-  }
-  return [endHour - startHour, endMin - startMin];
-};
 
 const App = ({
   title,
