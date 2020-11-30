910200affee609246c329382ea57cc245af4f7ba
pkiop <pkiop@naver.com>
2020-11-30 09:35:24 +0900

    Feat: Add timeInputComponent

---
+++ frontend/src/components/UI/molecules/TitleInput/index.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import TitleInput from '.';
+
+export default {
+  title: 'Molecules/TitleInput',
+  component: TitleInput,
+};
+
+export const HELLO = () => <TitleInput text={'Title'} value={'HELLO'} />;
