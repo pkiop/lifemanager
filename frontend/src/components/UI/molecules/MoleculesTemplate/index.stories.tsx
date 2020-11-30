40eff5a6887130e1e844801c8c4523c8c67c7c80
pkiop <pkiop@naver.com>
2020-11-30 09:34:53 +0900

    Feat: Add molecules story

---
+++ frontend/src/components/UI/molecules/MoleculesTemplate/index.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import MoleculesTemplate from '.';
+
+export default {
+  title: 'Molecules/MoleculesTemplate',
+  component: MoleculesTemplate,
+};
+
+export const Default = () => <MoleculesTemplate />;
