7e777a29258f69f41f12502d4bbc98b34e071057
pkiop <pkiop@naver.com>
2020-12-01 09:47:06 +0900

    Feat: Add TimeInput style, story

---
+++ frontend/src/components/UI/organisms/TimeInput/index.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TimeInput from '.';
+
+export default {
+  title: 'Organisms/TimeInput',
+  component: TimeInput,
+};
+
+export const Default = () => (
+  <TimeInput startTime={[11, 30]} endTime={[12, 40]} />
+);
---
+++ frontend/src/components/UI/organisms/TimeInput/style.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import TextComponent from '@Atoms/Text';
+import InputComponent from '@Atoms/Input';
+
+export const TimeInput = styled.div`
+  display: flex;
+`;
+
+export const Text = styled(TextComponent)``;
+
+export const TimeWrap = styled.div``;
+export const Wrap = styled.div`
+  display: flex;
+`;
+
+export const Input = styled(InputComponent)``;
+
+export default {};
