d51e07c4900286821a90e223d90887e3090df61a
pkiop <pkiop@naver.com>
2020-12-01 23:34:45 +0900

    Feat: Add toggle switch story

---
+++ frontend/src/components/UI/atoms/ToggleSwitch/index.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ToggleSwitch from '.';
+
+export default {
+  title: 'Atoms/ToggleSwitch',
+  component: ToggleSwitch,
+};
+
+export const Default = () => <ToggleSwitch text={'HELLO'} />;
