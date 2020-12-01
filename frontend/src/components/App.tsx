b06499b321cbd8178f4006452c7a0cc79f04f8c6
pkiop <pkiop@naver.com>
2020-12-01 23:33:13 +0900

    Fix: Delete fragment

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,29 +1,27 @@
-import React, { FC } from 'react';
+import React from 'react';
 import LoginPage from '@Components/pages/Login';
 import HomePage from '@Components/pages/Home';
 import AwayPage from '@Components/pages/Away';
 import NotFound from '@Components/pages/NotFound';
 import PrivateRoute from '@Components/UtilComponent/PrivateRouter';
-import GlobalStyleProvider from '@Styles/GlobalThemeProvider';
+import GlobalThemeProvider from '@Styles/GlobalThemeProvider';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const App: FC = () => (
-  <>
-    <GlobalStyleProvider>
-      <Router>
-        <Switch>
-          <PrivateRoute exact path="/">
-            <HomePage />
-          </PrivateRoute>
-          <PrivateRoute exact path="/away">
-            <AwayPage />
-          </PrivateRoute>
-          <Route path="/login" component={LoginPage} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </Router>
-    </GlobalStyleProvider>
-  </>
+const App = () => (
+  <GlobalThemeProvider>
+    <Router>
+      <Switch>
+        <PrivateRoute exact path="/">
+          <HomePage />
+        </PrivateRoute>
+        <PrivateRoute exact path="/away">
+          <AwayPage />
+        </PrivateRoute>
+        <Route path="/login" component={LoginPage} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
+  </GlobalThemeProvider>
 );
 
 export default App;
