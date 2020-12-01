5786cd1c69e04f2147982899805e6b4df1f3826a
pkiop <pkiop@naver.com>
2020-12-01 09:48:58 +0900

    Feat: Add labellist style

---
+++ frontend/src/components/UI/molecules/LabelList/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import LabelComponent from '@Atoms/Label';
+
+export const LabelList = styled.div``;
+
+export const Label = styled(LabelComponent)``;
+
+export const More = styled(LabelComponent)`
+  width: 2rem;
+  background-color: white;
+  color: black;
+`;
+
+export default {};
