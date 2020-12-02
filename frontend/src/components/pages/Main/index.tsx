44d9abd58b9a7bf1432a58f9b9485bdc688d12c8
pkiop <pkiop@naver.com>
2020-12-02 20:30:41 +0900

    Feat: Add mainpage

---
+++ frontend/src/components/pages/Main/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+import MainTemplate from '@Components/templates/MainTemplate';
+import RecodeList from '@Organisms/RecodeList';
+import Board from '@Organisms/Board';
+import BottomBtns from '@Molecules/BottomBtns';
+
+const App = ({ recodeList, className }: any) => {
+  const contents = (
+    <>
+      <Board />
+      <RecodeList recodeList={recodeList} />
+      <BottomBtns
+        lgText={'Add Recode'}
+        smText={'Finish'}
+        lgOnClick={() => alert('add')}
+        smOnClick={() => alert('finish')}
+      />
+    </>
+  );
+
+  return <MainTemplate contents={contents} className={className} />;
+};
+
+export default observer(App);
