46f377ac8e0cab0678e7b7108855a96f4e035bb1
pkiop <pkiop@naver.com>
2020-12-02 20:31:01 +0900

    Feat: Add maintemplate

---
+++ frontend/src/components/templates/MainTemplate/style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import HeaderBarComponent from '@Organisms/HeaderBar';
+
+export const MainTemplate = styled.div``;
+
+export const HeaderBar = styled(HeaderBarComponent)``;
