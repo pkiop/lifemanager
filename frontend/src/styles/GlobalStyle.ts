4462d044fe4e388c7acb049f0d9872028dc412df
pkiop <pkiop@naver.com>
2020-12-02 21:28:12 +0900

    Feat: Add force padding:0

--- frontend/src/styles/GlobalStyle.ts
+++ frontend/src/styles/GlobalStyle.ts
@@ -7,6 +7,7 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0 !important;
   }
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
