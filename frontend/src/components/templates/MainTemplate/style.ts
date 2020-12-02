618e094f91473968e2a52f01d4a9df7336da3233
pkiop <pkiop@naver.com>
2020-12-02 22:00:02 +0900

    Fix: Change template style

--- frontend/src/components/templates/MainTemplate/style.ts
+++ frontend/src/components/templates/MainTemplate/style.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import HeaderBarComponent from '@Organisms/HeaderBar';
 
-export const MainTemplate = styled.div``;
+export const MainTemplate = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 export const HeaderBar = styled(HeaderBarComponent)``;
