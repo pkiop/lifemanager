5dad590ee4e5b96d656268a71dc68dcc2bc487a0
pkiop <pkiop@naver.com>
2020-12-04 17:29:45 +0900

    Feat: Add OAuthRedirectPage

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -3,6 +3,7 @@ import LoginPage from '@Components/pages/Login';
 import HomePage from '@Components/pages/Home';
 import AwayPage from '@Components/pages/Away';
 import NotFound from '@Components/pages/NotFound';
+import OAuthCallbackPage from '@Pages/OAuthCallbackPage';
 import PrivateRoute from '@Components/UtilComponent/PrivateRouter';
 import GlobalThemeProvider from '@Styles/GlobalThemeProvider';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -17,6 +18,7 @@ const App = () => (
         <PrivateRoute exact path="/away">
           <AwayPage />
         </PrivateRoute>
+        <Route path="/login/oauthcallback" component={OAuthCallbackPage} />
         <Route path="/login" component={LoginPage} />
         <Route path="*" component={NotFound} />
       </Switch>
---
+++ frontend/src/components/pages/OAuthCallbackPage/index.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+
+const App = () => {
+  useEffect(() => {}, []);
+  return (
+    <>
+      <h1>OAuthRedirectUrl</h1>
+    </>
+  );
+};
+export default App;
