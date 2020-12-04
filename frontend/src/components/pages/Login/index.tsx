585ee9e90b7db72e28b3a58a8942c030edf04393
pkiop <pkiop@naver.com>
2020-12-04 22:17:00 +0900

    Feat: Add GitHub code redirect function

--- frontend/src/components/pages/Login/index.tsx
+++ frontend/src/components/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React from 'react';
 import axios from 'axios';
 import { observer } from 'mobx-react-lite';
 import GitHubLogo from '@Images/GitHub.png';
--- frontend/src/components/pages/OAuthCallbackPage/index.tsx
+++ frontend/src/components/pages/OAuthCallbackPage/index.tsx
@@ -1,11 +1,30 @@
-import React, { useEffect } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import qs from 'query-string';
 
-const App = () => {
-  useEffect(() => {}, []);
+const App = ({ location }: { location: any }) => {
+  const history = useHistory();
+  console.log('dodo');
+  const { code } = qs.parse(location.search);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getToken = async () => {
+      if (code == null) return;
+      setIsLoading(false);
+      const token = await axios.get(
+        `${process.env.APISERVER_HOST}/api/auth/github/getToken?code=${code}`,
+      );
+      setIsLoading(true);
+      history.push('/');
+    };
+    getToken();
+  }, []);
+
   return (
     <>
-      <h1>OAuthRedirectUrl</h1>
+      <h1>loading : {isLoading ? '로딩완료' : '로딩중'}</h1>
     </>
   );
 };
