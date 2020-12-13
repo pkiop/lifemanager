a4cb900bf2bddfca65b1a1667e2c9c0345a60857
pkiop <pkiop@naver.com>
2020-12-13 22:51:04 +0900

    Feat: Add redux async example and page

---
+++ frontend/src/components/pages/TestComp/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getOneUser } from 'modules/user';
+
+const App = () => {
+  const user = useSelector<any>((state: any) => state.user.user.title);
+  const dispatch = useDispatch();
+
+  dispatch(getOneUser(1));
+  console.log('render : ');
+  return (
+    <>
+      <div>temp</div>
+      <div>{JSON.stringify(user)}</div>
+    </>
+  );
+};
+
+export default App;
