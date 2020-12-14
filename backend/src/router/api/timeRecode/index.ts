fdc6dd632affe7d8c0ca90053f1b0ba5c713d09c
pkiop <pkiop@naver.com>
2020-12-15 08:30:21 +0900

    Fix: Add authorization middleware

--- backend/src/router/api/timeRecode/index.ts
+++ backend/src/router/api/timeRecode/index.ts
@@ -1,7 +1,9 @@
 import Router from 'koa-router';
 import timeRecodeController from 'controller/api/timeRecode';
+import { authorization } from 'middleware/auth';
 
 const router = new Router();
+router.use('/', authorization);
 router.get('/', timeRecodeController.get);
 router.post('/', timeRecodeController.post);
 router.put('/:id', timeRecodeController.put);
