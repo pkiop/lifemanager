ce2d30283f2e93664a16403ca2a83ae9c40096bf
pkiop <pkiop@naver.com>
2020-12-15 09:53:30 +0900

    Feat: Set Refs

--- frontend/src/components/UI/organisms/RecodeInput/index.tsx
+++ frontend/src/components/UI/organisms/RecodeInput/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { LabelType } from 'components/UI/atoms/Label';
+import { useSelector } from 'react-redux';
+import { ITimeRecode } from 'modules/timeRecode';
 import * as S from './style';
 
 export interface Props {
@@ -7,13 +9,35 @@ export interface Props {
   className?: string;
 }
 
-const App = ({ labelList, className }: Props) => (
-  <S.RecodeInput className={className}>
-    <S.TitleInput text="Title" />
-    <S.TimeInput />
-    <S.CategorySelectBar labelList={labelList} />
-    <S.BottomBtns />
-  </S.RecodeInput>
-);
+const App = ({ labelList, className }: Props) => {
+  const timeRecode: ITimeRecode = useSelector((state: any) => state.timeRecode.timeRecodeInput);
+  const titleRef = useRef<HTMLInputElement>(null);
+  const startHourRef = useRef<HTMLInputElement>(null);
+  const startMinRef = useRef<HTMLInputElement>(null);
+  const endHourRef = useRef<HTMLInputElement>(null);
+  const endMinRef = useRef<HTMLInputElement>(null);
+
+  const onclickHandler = () => {
+    console.log(titleRef.current && titleRef.current.value);
+    console.log(startHourRef.current && startHourRef.current.value);
+    console.log(startMinRef.current && startMinRef.current.value);
+    console.log(endHourRef.current && endHourRef.current.value);
+    console.log(endMinRef.current && endMinRef.current.value);
+  };
+  return (
+    <S.RecodeInput className={className}>
+      <S.TitleInput titleRef={titleRef} text={timeRecode.title} />
+      <S.TimeInput
+        startHourRef={startHourRef}
+        startMinRef={startMinRef}
+        endHourRef={endHourRef}
+        endMinRef={endMinRef}
+        startTime={timeRecode.startTime}
+        endTime={timeRecode.endTime}/>
+      <S.CategorySelectBar labelList={labelList} />
+      <S.BottomBtns lgOnClick={onclickHandler}/>
+    </S.RecodeInput>
+  );
+};
 
 export default App;
