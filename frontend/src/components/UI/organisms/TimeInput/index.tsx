645a69fe3c12b3cf2c693deceb0f04ef34f70050
pkiop <pkiop@naver.com>
2020-12-15 09:53:08 +0900

    Feat: Add Refs

--- frontend/src/components/UI/organisms/TimeInput/index.tsx
+++ frontend/src/components/UI/organisms/TimeInput/index.tsx
@@ -6,31 +6,39 @@ export interface Props {
   startTime?: number[];
   endTime?: number[];
   className?: string;
+  startHourRef?: any;
+  startMinRef?: any;
+  endHourRef?: any;
+  endMinRef?: any;
 }
 
-const App = ({ startTime = [0, 0], endTime = [0, 0], className }: Props) => {
+const App = ({
+  startTime = [0, 0],
+  endTime = [0, 0],
+  className, startHourRef, startMinRef, endHourRef, endMinRef, ...props
+}: Props) => {
   const [startHour, startMin] = startTime;
   const [endHour, endMin] = endTime;
   return (
-    <S.TimeInput className={className}>
+    <S.TimeInput className={className} {...props}>
       <S.TimeWrap>
         <S.Wrap>
           <S.Text text={'startHour'} />
-          <S.Input value={makeTwoNumber(startHour)} />
+          <S.Input inputRef={startHourRef} value={makeTwoNumber(startHour)} />
         </S.Wrap>
         <S.Wrap>
           <S.Text text={'startMin'} />
-          <S.Input value={makeTwoNumber(startMin)} />
+          <S.Input inputRef={startMinRef} value={makeTwoNumber(startMin)} />
         </S.Wrap>
       </S.TimeWrap>
       <S.TimeWrap>
         <S.Wrap>
           <S.Text text={'endHour'} />
-          <S.Input value={makeTwoNumber(endHour)} />
+          <S.Input inputRef={endHourRef} value={makeTwoNumber(endHour)} />
         </S.Wrap>
         <S.Wrap>
           <S.Text text={'endMin'} />
-          <S.Input value={makeTwoNumber(endMin)} />
+          <S.Input inputRef={endMinRef} value={makeTwoNumber(endMin)} />
         </S.Wrap>
       </S.TimeWrap>
     </S.TimeInput>
