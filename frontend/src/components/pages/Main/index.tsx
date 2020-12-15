92469fcb7ad3e554a92750ce08b71ae79af755ea
pkiop <pkiop@naver.com>
2020-12-15 09:54:24 +0900

    Feat: Add test category

--- frontend/src/components/pages/Main/index.tsx
+++ frontend/src/components/pages/Main/index.tsx
@@ -5,9 +5,29 @@ import Board from 'components/UI/organisms/Board';
 import BottomBtns from 'components/UI/molecules/BottomBtns';
 import { loadUser } from 'modules/user/index';
 import { useDispatch, useSelector } from 'react-redux';
+import RecodeInput from 'components/UI/organisms/RecodeInput';
 
+const TestLabelsForOverFlow = [
+  {
+    color: 'red',
+    children: 'red',
+  },
+  {
+    color: 'blue',
+    children: 'blue',
+  },
+  {
+    color: 'green',
+    children: 'green',
+  },
+  {
+    color: 'gray',
+    children: 'gray',
+  },
+];
 const App = () => {
   const user = useSelector<any>((state: any) => state.user.user && state.user.user.title);
+  const timeRecodes = useSelector<Array<any>>((state: any) => state.payload);
   const dispatch = useDispatch();
   dispatch(loadUser());
   useEffect(() => {
@@ -16,7 +36,8 @@ const App = () => {
     <>
       <div>user : {user}</div>
       <Board />
-      <RecodeList recodeList={[]} />
+      <RecodeInput labelList={TestLabelsForOverFlow} />
+      <RecodeList recodeList={timeRecodes as [] ? [] : []} />
       <BottomBtns
         lgText={'Add Recode'}
         smText={'Finish'}
