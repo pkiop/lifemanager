86edf2378a150796db45585a0278e143af8cf7b0
pkiop <pkiop@naver.com>
2020-12-31 23:33:57 +0900

    Feat: Add sample graphql

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,41 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+type Recode = {
+  userId: String;
+  title: String;
+  startTime: [number];
+  endTime?: [number];
+  category: String;
+  isActivate: Boolean;
+};
 const typeDefs = `
+
+  type Recode {
+    userId: String!,
+    title: String!,
+    startTime: [Int]!,
+    endTime: [Int],
+    category: String!,
+    isActivate: Boolean!
+  }
+
   type Query {
-    hello(name: String): String!
+    hello(name: String): String!,
+    bye(a: String, b:String): String!
+  }
+
+  type Mutation {
+    addRecode(userId: String): Boolean!
   }
 `;
 
 const resolvers = {
   Query: {
     hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
+    bye: (_: any, { a, b }: any) => `${a} ${b}`,
+  },
+  Mutation: {
+    addRecode: (userId: string) => true,
   },
 };
 
