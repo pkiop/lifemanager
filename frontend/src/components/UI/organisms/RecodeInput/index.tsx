5789ba7681a5cc90635cc1c43c6a40ed35049979
pkiop <pkiop@naver.com>
2021-01-04 02:43:45 +0900

    Feat: Add Recode appent feature

--- frontend/src/components/UI/organisms/RecodeInput/index.tsx
+++ frontend/src/components/UI/organisms/RecodeInput/index.tsx
@@ -1,8 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { LabelType } from 'components/UI/atoms/Label';
 // import { useDispatch, useSelector } from 'react-redux';
 import { setTimeRecode, ITimeRecode } from 'modules/timeRecode';
-
+import { IRecodeTime } from 'components/UI/organisms/TimeInput';
+import { gql, useQuery, useMutation } from '@apollo/client';
+import { addTimeRecode } from 'graphql/mutations';
 import * as S from './style';
 
 export interface Props {
@@ -11,36 +13,37 @@ export interface Props {
 }
 
 const App = ({ labelList, className }: Props) => {
-  // const timeRecode: ITimeRecode = useSelector((state: any) => state.timeRecode.timeRecodeInput);
-  // const dispatch = useDispatch();
   const titleRef = useRef<HTMLInputElement>(null);
   const startHourRef = useRef<HTMLInputElement>(null);
   const startMinRef = useRef<HTMLInputElement>(null);
   const endHourRef = useRef<HTMLInputElement>(null);
   const endMinRef = useRef<HTMLInputElement>(null);
-
+  const [addTimeRecodeMutation, { data }] = useMutation(gql`${addTimeRecode}`);
+  console.log('mutation result : ', data);
   const onclickHandler = () => {
-    console.log(titleRef.current && titleRef.current.value);
-    console.log(startHourRef.current && startHourRef.current.value);
-    console.log(startMinRef.current && startMinRef.current.value);
-    console.log(endHourRef.current && endHourRef.current.value);
-    console.log(endMinRef.current && endMinRef.current.value);
     const title = titleRef.current && titleRef.current.value;
     const startHour = startHourRef.current && startHourRef.current.value;
     const startMin = startMinRef.current && startMinRef.current.value;
-    const startTime = [startHour, startMin];
+    const startTime = {
+      hour: Number(startHour),
+      min: Number(startMin),
+    };
     const endHour = endHourRef.current && endHourRef.current.value;
     const endMin = endMinRef.current && endMinRef.current.value;
-    const endTime = [endHour, endMin];
-    // const recode = {
-    //   userId: 'pkiop',
-    //   title,
-    //   startTime,
-    //   endTime,
-    //   category: 'develop',
-    //   isActivate: true,
-    // };
-    // dispatch(setTimeRecode(recode));
+    const endTime = {
+      hour: Number(endHour),
+      min: Number(endMin),
+    };
+    addTimeRecodeMutation({
+      variables: {
+        userId: 'pkiop',
+        title,
+        startTime,
+        endTime,
+        category: 'develop',
+        isActive: true,
+      },
+    });
   };
 
   return (
@@ -51,8 +54,7 @@ const App = ({ labelList, className }: Props) => {
         startMinRef={startMinRef}
         endHourRef={endHourRef}
         endMinRef={endMinRef}
-        startTime={/* timeRecode.startTime */ [10, 20]}
-        endTime={/* timeRecode.endTime */ [20, 30]}/>
+      />
       <S.CategorySelectBar labelList={labelList} />
       <S.BottomBtns lgOnClick={onclickHandler}/>
     </S.RecodeInput>
