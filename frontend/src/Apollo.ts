e3dd1a229df53dee5fc297c1fc1da615c40f5b19
pkiop <pkiop@naver.com>
2021-01-07 20:32:21 +0900

    Feat: Add local schema

--- frontend/src/Apollo.ts
+++ frontend/src/Apollo.ts
@@ -1,8 +1,7 @@
 import { ApolloLink } from 'apollo-link';
 import { createAuthLink } from 'aws-appsync-auth-link';
 import { createHttpLink } from 'apollo-link-http';
-import { ApolloClient } from '@apollo/client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import appSyncConfig from 'aws-exports';
 
 const url = appSyncConfig.aws_appsync_graphqlEndpoint;
@@ -15,9 +14,26 @@ const link = ApolloLink.from([
   createAuthLink({ url, region, auth } as any) as any,
   createHttpLink({ uri: url }),
 ]);
+
+const typeDefs = gql`
+  extend type User {
+    username: String!
+  }
+
+  extend type Query {
+    getUser: User!
+  }
+
+  extend type Mutation {
+    setUser(username: String!): Boolean!
+  }
+
+`;
+
 const client = new ApolloClient({
   link,
   cache: new InMemoryCache(),
+  typeDefs,
 } as any);
 
 export default client;
