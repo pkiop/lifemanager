aba5621f6790208cad232dd828574f16b6cdc23a
pkiop <pkiop@naver.com>
2021-01-13 16:11:52 +0900

    Fix: Change timeinput css

--- frontend/src/components/UI/organisms/TimeInput/style.ts
+++ frontend/src/components/UI/organisms/TimeInput/style.ts
@@ -3,20 +3,42 @@ import TextComponent from 'components/UI/atoms/Text';
 import InputComponent from 'components/UI/atoms/Input';
 
 export const TimeInput = styled.div`
+  background-color: ${({ theme }) => theme.color.bage};
   display: flex;
+  justify-content: center;
+  border-radius: 0.5rem;
+  padding: 0 0.5rem;
+  box-sizing: border-box;
 `;
 
 export const Text = styled(TextComponent)`
-  width: 8em;
   text-align: center;
 `;
 
-export const TimeWrap = styled.div``;
+export const TimeWrap = styled.div`
+  box-sizing: border-box;
+  margin: 0.5rem 0.5rem;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  display: flex;
+  font-weight: 600;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.color.timeInput};
+`;
+
 export const Wrap = styled.div`
   display: flex;
+  justify-content: space-between;
+
+
+  :not(:last-child) {
+    margin-bottom: 0.4rem;
+  }
 `;
 
 export const Input = styled(InputComponent)`
+  width: 2.5rem;
+  font-size: 1rem;
   margin-left: 0.4em;
 `;
 
--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -1,6 +1,7 @@
 const color = {
   mainColor: '#2E4B73',
   bage: '#ECE6CC',
+  timeInput: '#F87878',
 };
 
 const theme = {
