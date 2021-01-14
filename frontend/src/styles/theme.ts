6c2dfef85a22af482c93fa193d2a108f0864bece
pkiop <pkiop@naver.com>
2021-01-15 00:05:03 +0900

    Feat: Add navbar height

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -1,6 +1,7 @@
 const color = {
   black: '#000000',
   mainColor: '#2E4B73',
+  white: '#FFFFFF',
   beige: '#ECE6CC',
   beigeCompare: '#E0980C',
   navbar: '#3F93FD',
@@ -17,6 +18,7 @@ const size = {
     height: '1.4rem',
   },
   mainInputRadius: '0.5rem',
+  navbarHeight: '3rem',
 };
 
 const theme = {
