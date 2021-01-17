45334eec5887ab24899d69942ad2a30db6b25e1a
pkiop <pkiop@naver.com>
2021-01-18 00:29:21 +0900

    Fix: Add Component name

--- frontend/src/components/templates/MainTemplate/index.tsx
+++ frontend/src/components/templates/MainTemplate/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import * as S from './style';
 
-function App({ contents, navPlusOnClick, className }: any) {
+function MainTemplate({
+  contents, navPlusOnClick, className,
+}: any) {
   return (
     <S.MainTemplate className={className}>
       <S.HeaderBar />
       <S.Contents>
         {contents}
       </S.Contents>
-      <S.NavBar navPlusOnClick={navPlusOnClick}/>
+      <S.NavBar navPlusOnClick={navPlusOnClick} />
     </S.MainTemplate>
   );
 }
 
-export default App;
+export default MainTemplate;
