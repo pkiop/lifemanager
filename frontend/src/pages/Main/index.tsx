70d215cc5090161ccee1f984b7a24d6578c7a284
pkiop <pkiop@naver.com>
2021-01-17 18:21:16 +0900

    Feat: Add update function

--- frontend/src/pages/Main/index.tsx
+++ frontend/src/pages/Main/index.tsx
@@ -1,7 +1,5 @@
-/* eslint-disable max-len */
 import React, { useCallback, useState, useReducer } from 'react';
 import MainTemplate from 'components/templates/MainTemplate';
-// import { IRecode } from 'components/UI/molecules/Recode';
 import RecodeList from 'components/UI/organisms/RecodeList';
 import { gql, useQuery } from '@apollo/client';
 import { listTimeRecodes } from 'graphql/queries';
@@ -27,22 +25,28 @@ const TestLabelsForOverFlow = [
 ];
 
 function App() {
+  const [clickedRecodeId, setClickedRecodeId] = useState<string>('');
+  const [bRecodeInput, toggleBRecodeInput] = useReducer((state: boolean) => !state, false);
+  const toggleRecodeInput = useCallback(() => toggleBRecodeInput(), []);
+
   const {
     loading, error, data, refetch,
   } = useQuery(gql`${listTimeRecodes}`);
-  const [bRecodeInput, toggleBRecodeInput] = useReducer((state: boolean) => !state, false);
-  // const [clickedRecode, setClickedRecode] = useState<IRecode>();
-  const navPlusOnClick = useCallback(() => toggleBRecodeInput(), []);
 
   const contents = (
     <>
-      <RecodeList timeRecodes={data?.listTimeRecodes?.items} loading={loading} error={error} />
-      <S.RecodeInput labelList={TestLabelsForOverFlow} refetch={refetch} className={bRecodeInput ? 'active' : ''}/>
-      <S.RecodeInputCover onClick={navPlusOnClick} className={bRecodeInput ? 'active' : ''} />
+      <RecodeList
+        setUpdateRecodeId={setClickedRecodeId}
+        timeRecodes={data?.listTimeRecodes?.items}
+        toggleRecodeInput={toggleRecodeInput}
+        loading={loading}
+        error={error} />
+      <S.RecodeInput recodeId={clickedRecodeId} labelList={TestLabelsForOverFlow} refetch={refetch} className={bRecodeInput ? 'active' : ''}/>
+      <S.RecodeInputCover onClick={toggleRecodeInput} className={bRecodeInput ? 'active' : ''} />
     </>
   );
 
-  return <MainTemplate contents={contents} navPlusOnClick={navPlusOnClick}/>;
+  return <MainTemplate contents={contents} navPlusOnClick={toggleRecodeInput}/>;
 }
 
 export default App;
