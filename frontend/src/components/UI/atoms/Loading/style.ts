b6eb49eb70fd9d80534cc424e901419b7e150a0e
pkiop <pkiop@naver.com>
2021-01-19 21:04:54 +0900

    Feat: Add loadingpage using react-spinners

---
+++ frontend/src/components/UI/atoms/Loading/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrap = styled.div`
+  margin-top: 15rem;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default {};
