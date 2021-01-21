7caa1b8bc317203d563f6a6ca2027232a19703e1
pkiop <pkiop@naver.com>
2021-01-21 15:55:23 +0900

    Feat: Add unique selected state for category

--- frontend/src/components/UI/molecules/LabelList/index.tsx
+++ frontend/src/components/UI/molecules/LabelList/index.tsx
@@ -3,16 +3,32 @@ import { LabelType } from 'components/UI/atoms/Label';
 import * as S from './style';
 
 export interface Props {
+  selectedCategory?: string;
   labelList: LabelType[];
+  setSelectedCategory ?: any;
   className?: string;
 }
 
-const mapFunc = (label: LabelType) => (
-  <S.Label key={label.color + label.labelName} color={label.color} labelName={label.labelName} />
+const mapFunc = (selectedCategory:string, setSelectedCategory: any) => (label: LabelType) => (
+  <S.Label
+    key={label.color + label.labelName}
+    color={label.color}
+    onClick={(e: any) => {
+      [...e.target.parentNode.childNodes].forEach((el: any) => {
+        el.classList.remove('active');
+      });
+      e.target.classList.add('active');
+      setSelectedCategory(label.labelName);
+    }}
+    labelName={label.labelName}
+    className={selectedCategory === label.labelName ? 'active' : ''}
+  />
 );
 
-function LabelList({ labelList, className }: Props) {
-  const labelComponentList = labelList.map(mapFunc);
+function LabelList({
+  labelList, selectedCategory, setSelectedCategory, className,
+}: Props) {
+  const labelComponentList = labelList.map(mapFunc(selectedCategory || '', setSelectedCategory));
   if (labelList.length > 3) {
     return (
       <S.LabelList className={className}>
