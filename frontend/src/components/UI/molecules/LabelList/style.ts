d4cef2f0a3e3531477e2292229e9e82fb032b3ce
pkiop <pkiop@naver.com>
2021-01-21 18:46:09 +0900

    Feat: Style overflow labels

--- frontend/src/components/UI/molecules/LabelList/style.ts
+++ frontend/src/components/UI/molecules/LabelList/style.ts
@@ -2,20 +2,31 @@ import styled from 'styled-components';
 import LabelComponent from 'components/UI/atoms/Label';
 
 export const LabelList = styled.div`
+  position: relative;
   display: flex;
   justify-content: space-between;
 `;
 
 export const Label = styled(LabelComponent)`
-  :not(:last-child) {
-    margin-right: 0.5rem;
-  }
+  margin-right: 0.5rem;
   &.active {
     color: yellow;
+  }
+  &.overflow {
+    display: none;
+    &.overflow.visible {
+      position: absolute;
+      top: -${({ idx }: any) => idx}rem;
+      left: 10rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
   }
 `;
 
 export const More = styled(LabelComponent)`
+  margin-right: 0.5rem;
   width: 2rem;
   background-color: white;
   color: black;
