cbab5b40235f7a028cdd0c7d623027e5418a6552
pkiop <pkiop@naver.com>
2021-03-26 18:41:53 +0900

    Api: Fix lint error

--- frontend/src/libs/api.ts
+++ frontend/src/libs/api.ts
@@ -1,34 +1,21 @@
 import axios from './axios';
 
 // TODO: Delete
-export const getOneUser = (id: number) => {
-  axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
-};
+export const getOneUser = (id: number) => axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
 
 // TODO: Delete
-export const getUsers = () => {
-  axios.get('https://jsonplaceholder.typicode.com/users');
-};
+export const getUsers = () => axios.get('https://jsonplaceholder.typicode.com/users');
 
-// TODO: Delete
-export const getUsersByAccessToken = () => {
-  axios.get(`${process.env.REACT_APP_APISERVER_HOST}/api/user/current`);
-};
+const apiUrl = process.env.REACT_APP_APISERVER_HOST;
 
-export const getTokenAndUser = (code: string) => {
-  axios.get(
-    `${process.env.REACT_APP_APISERVER_HOST}/api/auth/github/getToken?code=${code}`,
-  );
-};
+export const getUsersByAccessToken = () => axios.get(`${apiUrl}/api/user/current`);
 
-export const authentication = () => {
-  axios.get(`${process.env.REACT_APP_APISERVER_HOST}/auth/authentication`);
-};
+export const getTokenAndUser = (code: string) => axios.get(
+  `${apiUrl}/api/auth/github/getToken?code=${code}`,
+);
 
-export const getTimeRecode = () => {
-  axios.get(`${process.env.REACT_APP_APISERVER_HOST}/api/timeRecode`);
-};
+export const authentication = () => axios.get(`${apiUrl}/auth/authentication`);
 
-export const postTimeRecode = (payload: any) => {
-  axios.post(`${process.env.REACT_APP_APISERVER_HOST}/api/timeRecode`, payload);
-};
+export const getTimeRecode = () => axios.get(`${apiUrl}/api/timeRecode`);
+
+export const postTimeRecode = (payload: any) => axios.post(`${apiUrl}/api/timeRecode`, payload);
